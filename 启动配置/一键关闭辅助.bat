444a53c6b9ca212fa24b0f3ab0abe9eb954c2694
wangzhuoqi <wangzhuoqi@tdwaji.com>
2022-03-08 18:43:31 +0800

    增加一键关闭

---
+++ 启动配置/一键关闭辅助.bat
@@ -0,0 +1,6 @@
+taskkill /f /im:CGAssistant.exe
+
+taskkill /f /im:MLAssist.exe
+
+taskkill /f /im:node.exe
+
---
+++ 启动配置/一键关闭魔力.bat
@@ -0,0 +1,7 @@
+taskkill /f /im:CGAssistant.exe
+
+taskkill /f /im:MLAssist.exe
+
+taskkill /f /im:node.exe
+
+taskkill /f /im:cg_item_6000.exe
