8ace9802f55c566678aa2978f27f08ac1ebe694a
wangzhuoqi <wangzhuoqi@tdwaji.com>
2022-04-24 18:42:25 +0800

    增加在线乞讨

--- 脚本/★公益/飞碟治疗.lua
+++ 脚本/★公益/飞碟治疗.lua
@@ -29,13 +29,34 @@ function main()
 	移动(34,89)
 	回复(1)			-- 转向北边恢复人宠血魔
     移动(28, 85)  
+	common.checkGold(身上最少金币,身上最多金币,身上预置金币)	
+	if(人物("金币")<5000)then		
+		goto reqHelp	
+	end
 	goto scriptstart
 ::scriptstart::	
 	工作("治疗",4)     
 	if(人物("魔") < 补魔值)then goto LiBao end		
-	if(取当前地图名() ~= "里谢里雅堡") then goto begin end
-	common.checkGold(身上最少金币,身上最多金币,身上预置金币)
-	等待(2000)
+	if(取当前地图名() ~= "里谢里雅堡") then goto begin end	
+	等待(5000)
 	goto scriptstart  
+::reqHelp::
+	if(取当前地图名() ~= "里谢里雅堡")then
+		回城()
+		等待到指定地图("艾尔莎岛")
+		移动(140,105)
+		转向(1, "")
+		等待服务器返回()
+		对话选择("4", "", "")
+		等待到指定地图("里谢里雅堡")	
+		移动(28, 85)  
+	end
+	日志("我是公益医生，身上没有补魔钱了，请点我交易，当前金币"..人物("金币"),1)
+	等待交易("","","",10000)
+	if(人物("金币") > 5000)then
+		日志("当前金币"..人物("金币").."超5000，开始继续提供服务",1)
+		goto scriptstart
+	end	
+	goto reqHelp	
 end
 main()
